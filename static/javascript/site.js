270216c9ea56e1291436fb22aedbc89b363fb3bb
mmarol <mmarol01@risd.edu>
2017-06-08 14:18:45 -0400

    calendar filter is now remembered

--- static/javascript/site.js
+++ static/javascript/site.js
@@ -10147,23 +10147,67 @@ var $ = global.jQuery;
 module.exports = ScheduleToggle;
 
 function ScheduleToggle() {
-    if (!(this instanceof ScheduleToggle)) {
-        return new ScheduleToggle();
-    }
+  if (!(this instanceof ScheduleToggle)) {
+      return new ScheduleToggle();
+  }
 
-    console.log('ScheduleToggle initialized.');
+  console.log('ScheduleToggle initialized.');
 
-    dayToggle('friday');
-    dayToggle('saturday');
-    dayToggle('sunday');
+  var selectedFilter;
+  var currentFilter;
+  var lastFilter = window.localStorage.getItem('lastFilter');
+  var filterHash;
+  var $filterButton;
+  var $filteredItems;
+  var $filteredColumn;
 
-    function dayToggle(day) {
-      console.log(day + " dayToggle intiated.");
-      $('#calendar-box__button--' + day).click(function() {
-        $(this).addClass('calendar-box__button--active').siblings().removeClass('calendar-box__button--active');
-        $('#calendar-box__column--' + day).removeClass('calendar-box__column--hidden').siblings().not('calendar-box__toggle').addClass('calendar-box__column--hidden');
-      });
-    }
+/*
+  dayToggle('friday');
+  dayToggle('saturday');
+  dayToggle('sunday');
+
+  function dayToggle(day) {
+    console.log(day + " dayToggle intiated.");
+    $('#calendar-box__button--' + day).click(function() {
+      $(this).addClass('calendar-box__button--active').siblings().removeClass('calendar-box__button--active');
+      $('#calendar-box__column--' + day).removeClass('calendar-box__column--hidden').siblings().not('calendar-box__toggle').addClass('calendar-box__column--hidden');
+    });
+  }
+*/
+
+  $('.calendar-box__button').click(function() {
+    setAudienceFilter($(this));
+    filterAudiences(currentFilter);
+  });
+
+
+  if (lastFilter) {
+    console.log(lastFilter);
+    filterAudiences(lastFilter);
+  }
+
+  function setAudienceFilter(element) {
+
+    $(element).addClass('active').siblings().removeClass('active');
+
+    selectedFilter = $(element).data('filter');
+    selectedFilter = selectedFilter.replace('\#','');
+
+    window.localStorage.setItem('lastFilter', selectedFilter);
+    currentFilter = window.localStorage.getItem('lastFilter');
+
+  }
+
+  function filterAudiences(filter) {
+
+    $filterButton = $('#calendar-box__button--' + filter);
+    $filteredColumn = $('#calendar-box__column--' + filter);
+
+    $($filterButton).addClass('active').siblings().removeClass('active');
+    $('.calendar__audience').removeClass('initial');
+    $('.calendar-box__column').addClass('calendar-box__column--hidden');
+    $filteredColumn.removeClass('calendar-box__column--hidden');
+  }
 
 }
 
