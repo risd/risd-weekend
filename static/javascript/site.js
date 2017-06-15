0f07be6b3c81e39710a02d5c856b4c0e6a982710
mmarol <mmarol01@risd.edu>
2017-06-15 12:11:53 -0400

    updated modal url behavior
    - still need to fine tune the back button behavior

--- static/javascript/site.js
+++ static/javascript/site.js
@@ -9878,9 +9878,10 @@ function EventModal(opts) {
   var $events = $('.calendar-box__item').has('.calendar-box__item-toggle');
   var $featuredEvents = $('.featured-events__item');
   var $modalContainer = $('.modal__container');
-  var $modalToggle = $('.modal__item-toggle');
+  var $modalToggle = $('.modal__item-toggle, .modal__background');
   var $activeEvent;
-  var lastHash;
+  var modalID;
+  var historyState;
 
   openModal($(this));
 
@@ -9915,29 +9916,35 @@ function EventModal(opts) {
   });
 
   function openModal(activeEvent) {
-    if (activeEvent.data('modal-source-id')) {
-      var historyState = '#' + activeEvent.data('modal-source-id');
+    /*
+      Case 1: Clicked element has a modal source ID
+      Case 2: On load, page has a hash value
+      Case 3: 1 and 2 are false.
+    */
+    if (activeEvent.data('modal-source-id') !== undefined) {
+      modalID = activeEvent.data('modal-source-id');
+      historyState = '#' + activeEvent.data('modal-source-id');
       history.pushState({}, '', historyState);
-      if (activeEvent.data('modal-source-id')) {
-        modalID = activeEvent.data('modal-source-id');
-      } else {
-        modalID = window.location.hash.replace('#','');
-      }
+    } else if(window.location.hash) {
+      modalID = window.location.hash.replace('#','');
+    } else {
+      modalID = false;
+    }
 
+    if (modalID !== false) {
       modalTarget = $(".modal__item[data-modal-target-id='" + modalID +"']");
 
       if ($('.modal__container').has(modalTarget).length) {
-        $('.modal__container').addClass('modal--fade_in');
+        $('.modal__container').addClass('modal--slide_in');
         modalTarget.addClass('modal--on');
         $('body').addClass('modal__body--noscroll');
       }
-
-      lastHash = window.location.hash;
     }
+
   }
 
   function closeModal() {
-    $('.modal__container').removeClass('modal--fade_in');
+    $('.modal__container').removeClass('modal--slide_in');
     $('.modal__item').removeClass('modal--on');
     $('body').removeClass('modal__body--noscroll');
   }
