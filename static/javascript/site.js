f4e9748c37212c87e83964072906ed803483d8ce
rubonics <rubenrodriguez+github@gmail.com>
2019-07-26 20:16:38 -0400

    [static] adds built assets

--- static/javascript/site.js
+++ static/javascript/site.js
@@ -10522,6 +10522,164 @@ function EventModal(opts) {
 (function (global){
 global.jQuery = global.$ = require('jquery');
 
+module.exports = Galleries;
+
+function Galleries ( options ) {
+  if ( ! ( this instanceof Galleries ) ) return new Galleries( options )
+
+  if ( ! options ) {
+    console.log( 'Pass in an options object : { selector, attribute } to initialize galleries.' )
+    return;
+  }
+
+  var selector = options.selector
+  var attribute = options.attribute
+  var initialDelay = options.intialDelay || 500
+  var duration = options.duration || 1500
+  var ERROR_VALUE = undefined
+
+  $( selector )
+    .map( extractSpec( { attribute: attribute, errorValue: ERROR_VALUE } ) )
+    .get() // returns a regular object, switching back to a signature of ( data, index ) instead of ( index, data )
+    .filter( exclude( ERROR_VALUE ) )
+    .forEach( cycleManager( { initialDelay: initialDelay, duration: duration } ) )
+}
+
+function exclude ( excludeValue ) {
+  return function filterExcludeValue ( value ) {
+    return excludeValue !== value;
+  }
+}
+
+function extractSpec ( options ) {
+  var attribute = options.attribute
+  var errorValue = options.errorValue
+
+  return extractSpecForElement;
+
+  function extractSpecForElement ( index, element ) {
+    $element = $( element )
+    try {
+      var specs = JSON.parse( $element.attr( attribute ) )
+    } catch ( error ) {
+      console.log( 'Could not initalize gallery at:' )
+      console.log( element )
+      return errorValue;
+    }
+    // must wrap in an array for jquery to treat as a normal array
+    // even though the specs object is an array already
+    return [specs];
+  }
+}
+
+function cycleManager ( options ) {
+  var initialDelay = options.initialDelay
+  var duration = options.duration
+
+  return cycleManagerForSpecs;
+
+  function cycleManagerForSpecs ( specs ) {
+    var previous = false
+    var current = 0
+
+    // nothing to cycle through
+    if ( specs.length === 1 ) return;
+
+    setTimeout( initialFrame, initialDelay )
+
+    function initialFrame () {
+      setInterval( showNext, duration )
+    }
+
+    function showNext () {
+      previous = nextIndex( previous )
+      current = nextIndex( current )
+      hideSpec( specs[ previous ] )
+      showSpec( specs[ current ] )
+    }
+
+    function nextIndex ( index ) {
+      if ( index === false ) {
+        return 0
+      }
+      else if ( index === specs.length - 1 ) {
+        return 0
+      }
+      else {
+        return index += 1
+      }
+    }
+
+  }
+}
+
+
+/**
+ * spec : [ {
+ *    background_image?: {
+ *      querySelector: '',
+ *      class: '',
+ *      image: { resize_url }
+ *    },
+ *    color?: {
+ *      querySelector: '',
+ *      class: '',
+ *      classValue: '',
+ *    }
+ *  } ]
+ */
+
+function showSpec ( spec ) {
+  if ( validSpec( spec.background_image ) ) {
+    showToggleClassSpec( spec.background_image )
+  }
+  if ( validSpec( spec.color ) ) {
+    showToggleClassSpec( spec.color )
+  }
+}
+
+function hideSpec ( spec ) {
+  if ( validSpec( spec.background_image ) ) {
+    hideToggleClassSpec( spec.background_image )
+  }
+  if ( validSpec( spec.color ) ) {
+    hideToggleClassSpec( spec.color )
+  }
+}
+
+function showToggleClassSpec ( toggleClassSpec ) {
+  // $( toggleClassSpec.querySelector ).addClass( toggleClassSpec.class )
+  document
+    .querySelectorAll( toggleClassSpec.querySelector )
+    .forEach( addClass )
+
+  function addClass ( element ) {
+    element.classList.add( toggleClassSpec.class )
+  }
+}
+
+function hideToggleClassSpec ( toggleClassSpec ) {
+  // $( toggleClassSpec.querySelector ).removeClass( toggleClassSpec.class )
+  document
+    .querySelectorAll( toggleClassSpec.querySelector )
+    .forEach( removeClass )
+
+  function removeClass ( element ) {
+    element.classList.remove( toggleClassSpec.class )
+  }
+}
+
+function validSpec ( spec ) {
+  return spec && spec.querySelector && spec.class
+    ? true
+    : false
+}
+
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{"jquery":1}],5:[function(require,module,exports){
+(function (global){
+global.jQuery = global.$ = require('jquery');
+
 var mobileMenuToggle = require('./mobileMenuToggle.js')();
 var scrollAnchor = require('./scrollAnchor.js')();
 var activeNav = require('./activeNav.js')();
@@ -10529,10 +10687,12 @@ var scheduleToggle = require('./scheduleToggle.js')();
 var scheduleHover = require('./scheduleHover.js')();
 var livestream = require('./livestream.js')();
 var eventModal = require('./eventModal.js')();
-var preloadImages = require('./preloadImages.js')($);
-
+var galleries = require('./galleries')({
+  selector: '[data-gallery-spec]',
+  attribute: 'data-gallery-spec',
+})
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"./activeNav.js":2,"./eventModal.js":3,"./livestream.js":5,"./mobileMenuToggle.js":6,"./preloadImages.js":7,"./scheduleHover.js":8,"./scheduleToggle.js":9,"./scrollAnchor.js":10,"jquery":1}],5:[function(require,module,exports){
+},{"./activeNav.js":2,"./eventModal.js":3,"./galleries":4,"./livestream.js":6,"./mobileMenuToggle.js":7,"./scheduleHover.js":8,"./scheduleToggle.js":9,"./scrollAnchor.js":10,"jquery":1}],6:[function(require,module,exports){
 (function (global){
 var $ = global.jQuery;
 
@@ -10566,7 +10726,7 @@ function Livestream(opts) {
 }
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{}],6:[function(require,module,exports){
+},{}],7:[function(require,module,exports){
 (function (global){
 var $ = global.jQuery;
 // Modernizr is being used as a global variable
@@ -10653,37 +10813,6 @@ function MobileMenuToggle() {
 }
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{}],7:[function(require,module,exports){
-module.exports = function( $ ){
-
-  // console.log('PreLoadImages initialized.');
-
-  var modifiers = JSON.parse($("#modifiers-json").html());
-  var modifierImageUrls = [];
-  var imagesToLoad = [];
-
-  if (modifiers) {
-    getImages();
-    preloadImages(modifierImageUrls);
-  }
-
-  function getImages() {
-    for (var i = 0; i < modifiers.length; i++) {
-      if (modifiers[i].modifier_image) {
-        modifierImageUrls.push(modifiers[i].modifier_image.resize_url);
-      }
-    }
-  }
-
-  function preloadImages(imageArray) {
-    for (i = 0; i < imageArray.length; i++) {
-			imagesToLoad[i] = new Image();
-			imagesToLoad[i].src = imageArray[i];
-		}
-  }
-
-};
-
 },{}],8:[function(require,module,exports){
 (function (global){
 var $ = global.jQuery;
@@ -10839,4 +10968,4 @@ function ScrollAnchorTest() {
 }
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{}]},{},[4]);
+},{}]},{},[5]);
